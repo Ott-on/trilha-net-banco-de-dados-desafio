d4e44259a6cac199dc99f5f06d2945b1356547d9
Ott-on <ottonvinicius1004@gmail.com>
2024-05-27 22:01:48 -0300

    fix: COUNT

--- SQLQuerysDesafio.sql
+++ SQLQuerysDesafio.sql
@@ -60,7 +60,7 @@ ORDER BY
 -- 7 - Buscar a quantidade de filmes lançadas no ano, agrupando por ano, ordenando pela duracao em ordem decrescente
 SELECT 
 	Ano,
-	COUNT(*) Quantidade
+	COUNT(Ano) Quantidade
 FROM
 	Filmes
 GROUP BY
